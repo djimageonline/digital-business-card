bc7baae617fe70da02ffbc1eb2660e792aa35273
Johnny Proano <jproano22@yahoo.com>
2023-05-15 12:36:31 -0400

    added info component with img, title, email and linked in button

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,7 +1,14 @@
 export default function Info() {
   return (
     <div>
-      <img src="src/assets/Headshot.png"></img>
+      <img src="src/assets/Headshot.png" className="headshot"></img>
+      <h1>Johnny Proano</h1>
+      <p>Software Engineer</p>
+      <p>https://www.johnnyproano.com/</p>
+      <div className="btn-container">
+        <button className="btn-email">Email</button>
+        <button className="btn-linkedin">LinkedIn</button>
+      </div>
     </div>
   );
 }
